5c5b003bdf56b7b5a61623af468972045fda2e0d
hack <hack@hack>
2016-08-26 17:26:23 -0300

    Projeto Storm

---
+++ src/storm/Login.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package storm;
+
+/**
+ *
+ * @author hack
+ */
+public class Login {
+    private String usuario;
+    private String senha;
+    
+  private void Logar(String usuario,String senha)
+  {
+  
+  }
+  
+  
+  private void AlterarSenha(){
+  
+  }
+  
+  private void Cadastrar(){
+  
+  }
+    
+    
+}
