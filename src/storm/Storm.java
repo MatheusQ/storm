9e48e7f9ea634b61a8886495a730d93d7abf1eac
hack <hack@hack>
2016-09-03 17:32:00 -0300

    Projeto Storm Interface login interface Alterar senha

--- src/storm/Storm.java
+++ src/storm/Storm.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package storm;
+import Interface.Login;
 
 /**
  *
@@ -16,6 +17,9 @@ public class Storm {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+            Login login = new Login();  
+      
+            login.setVisible(true);  
     }
     
 }
